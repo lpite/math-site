df343e9e401f83ec874baea1894e910874e473ba
lpite <farionsasa@gmail.com>
2023-02-13 22:29:11 +0200

    remove readme

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,28 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-      solidPlugin(), 
-      VitePWA({ registerType: 'autoUpdate', })
-    ],
+    solidPlugin(), 
+    VitePWA({
+      registerType: 'autoUpdate', manifest: {
+        name: 'Math questions',
+        short_name: 'Math',
+        description: 'Math questions',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     port: 3000,
   },
