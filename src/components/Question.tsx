a7a4506fe55247a519ee32d9362f8fc0a7f989d1
lpite <farionsasa@gmail.com>
2023-02-23 19:04:29 +0200

    Question Difficulty

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,19 +1,21 @@
-import { createEffect, createSignal, For } from "solid-js";
+import { createEffect, createSignal, For, onMount } from "solid-js";
 import { random } from "../utils/random";
 import shuffleArray from "../utils/shuffleArray";
 
 import {
 	useNotification
 } from "../stores/notificationStore"
+import { useQuestion } from "../stores/questionStore";
 
 const ANSWERS_COUNT = 4;
 
-function generateQuestion() {
+function generateQuestion(difficultyLevel: number = 1) {
 	const questionType = random(1, 4).toString();
-	console.log(questionType)
+	const difficulty = difficultyLevel * 3;
+
 	if (questionType === "1") {
-		const firstNumber = random(1, 100);
-		const secondNumber = random(1, 100)
+		const firstNumber = random(1, 100 + difficulty);
+		const secondNumber = random(1, 100 + difficulty)
 		const answer = firstNumber +
 			secondNumber
 		return {
@@ -22,8 +24,8 @@ function generateQuestion() {
 		};
 	}
 	if (questionType === "2") {
-		const firstNumber = random(1, 100);
-		const secondNumber = random(1, 100)
+		const firstNumber = random(1, 100 + difficulty);
+		const secondNumber = random(1, 100 + difficulty)
 		const answer = secondNumber
 		return {
 			answer: answer,
@@ -31,8 +33,8 @@ function generateQuestion() {
 		};
 	}
 	if (questionType === "3") {
-		const firstNumber = random(1, 15);
-		const secondNumber = random(1, 15)
+		const firstNumber = random(1, 15 + (difficulty / 2));
+		const secondNumber = random(1, 15 + (difficulty / 2))
 		const answer = firstNumber * secondNumber
 		return {
 			answer: answer,
@@ -40,8 +42,8 @@ function generateQuestion() {
 		};
 	}
 	if (questionType === "4") {
-		const firstNumber = random(1, 15);
-		const secondNumber = random(1, 15)
+		const firstNumber = random(1, 15 + (difficulty / 2));
+		const secondNumber = random(1, 15 + (difficulty / 2))
 		const answer = (firstNumber * secondNumber) / firstNumber
 		return {
 			answer: answer,
@@ -75,20 +77,23 @@ function generateWrongAnswers(answer: number) {
 	return shuffleArray(answerVarinats) as number[];
 }
 export default function Question() {
-	const [question, setQuestion] = createSignal(generateQuestion());
+	const question = useQuestion()
 	const [selectedAnswer, setSelectedAnswer] = createSignal<number | null>(null);
 
 	const notification = useNotification()
 
 	function answerQuestion() {
-		if (selectedAnswer() === question().answer) {
-			notification.setText("yep")
+		if (selectedAnswer() === question.answer) {
+			notification.setText(`yep new difficulty ${question.questionDifficulty + 1}`)
+			question.setDifficulty(question.questionDifficulty + 1)
 		} else {
 			notification.setText("noup")
+			question.setDifficulty(1)
 		}
 		notification.showNotification()
-
-		setQuestion(generateQuestion())
+		const newQuestion = generateQuestion(question.questionDifficulty);
+		question.setQuestionText(newQuestion.questionText);
+		question.setAnswer(newQuestion.answer)
 		setSelectedAnswer(null)
 	}
 	function selectAnswer(answer: number) {
@@ -101,16 +106,22 @@ export default function Question() {
 			setTimeout(() => notification.hideNotification(), 1000)
 		}
 	})
+	onMount(() => {
+		const newQuestion = generateQuestion()
+		question.setQuestionText(newQuestion.questionText)
+		question.setAnswer(newQuestion.answer)
+
+	})
 	return (
 		<>
 
 			<div class="question_block">
 				<span class="question_block__question_text">
-					{question().questionText}
+					{question.questionText}
 				</span>
 			</div>
 			<div class="answers_block">
-				<For each={generateWrongAnswers(question().answer)}>
+				<For each={generateWrongAnswers(question.answer)}>
 					{(answer) => (
 						<>
 							<label class="answers_block__answer" 
