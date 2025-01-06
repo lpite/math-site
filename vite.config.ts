7f8e593409cb9a99f4df565a7b893b82eb514ae2
lpite <farionsasa@gmail.com>
2025-01-06 19:50:24 +0200

    fix theme

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,38 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
-import solidPlugin from 'vite-plugin-solid';
-import { VitePWA } from 'vite-plugin-pwa'
+import solidPlugin from "vite-plugin-solid";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
-    solidPlugin(), 
+    solidPlugin(),
     VitePWA({
-      registerType: 'autoUpdate', manifest: {
-        name: 'Math questions',
-        short_name: 'Math',
-        description: 'Math questions',
-        theme_color: '#000000',
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Math questions",
+        short_name: "Math",
+        description: "Math questions",
+        theme_color: "#000000",
+        background_color: "#000000",
         icons: [
           {
-            src: 'logo.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "logo.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'logo.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+            src: "logo.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
   server: {
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
